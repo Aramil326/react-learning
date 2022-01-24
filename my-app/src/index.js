4f68eafa9d9e3321a9a0c94d8345a689bb7eb7d5
Shaimardanov Aramil <aramil28022005@gmail.com>
2022-01-24 20:26:33 +0300

    js 88-89 lessons. Strict mode, creating components

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,19 +1,12 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
-const text = 'Hello, world';
-
-const elem = (
-  <div>
-    <h2>{text}</h2>
-    <input type="text" />
-    <button />
-  </div>
-);
+import { Header } from './App';
 
 ReactDOM.render(
-  elem,
+  <StrictMode>
+    <App />
+  </StrictMode>,
   document.getElementById('root')
 );
