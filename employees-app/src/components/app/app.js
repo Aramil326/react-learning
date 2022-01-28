0331e6d4893d8b5ab2c9e2d14949f43cf5041f81
Shaimardanov Aramil <aramil28022005@gmail.com>
2022-01-28 18:48:59 +0300

    js 99 lesson. State Immutability and Custom Events

--- employees-app/src/components/app/app.js
+++ employees-app/src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,26 +8,53 @@ import EmployeesAddForm from '../employee-add-form/employee-add-form';
 
 import './app.css';
 
-function App() {
-  const data = [
-    { name: 'John S.', salary: 800, increase: false, id: 1 },
-    { name: 'Alex D.', salary: 3000, increase: true, id: 2 },
-    { name: 'Leise H.', salary: 155000, increase: false, id: 3 }
-  ];
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { name: 'John S.', salary: 800, increase: false, id: 1 },
+        { name: 'Alex D.', salary: 3000, increase: true, id: 2 },
+        { name: 'Leise H.', salary: 155000, increase: false, id: 3 }
+      ]
+    }
+  }
 
-  return (
-    <div className="app">
-      <AppInfo />
+  deleteItem = (id) => {
+    this.setState(({ data }) => {
+      // const index = data.findIndex(elem => elem.id === id);
 
-      <div className="search-panel">
-        <SearchPanel />
-        <AppFilter />
+      // const before = data.slice(0, index)
+      // const after = data.slice(index + 1)
+
+      // const newArr = [...before, ...after]
+
+      return {
+        data: data.filter(item => item.id !== id)
+      }
+    })
+  }
+
+
+
+  render() {
+    return (
+      <div className="app">
+        <AppInfo />
+
+        <div className="search-panel">
+          <SearchPanel />
+          <AppFilter />
+        </div>
+
+        <EmployeeList
+          data={this.state.data}
+          onDelete={this.deleteItem}
+        />
+        <EmployeesAddForm />
       </div>
-
-      <EmployeeList data={data} />
-      <EmployeesAddForm />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
