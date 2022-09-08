d8b363cad76fb0ef211ee4a1077171037e29194e
Shaimardanov Aramil <aramil28022005@gmail.com>
2022-09-08 18:00:14 +0300

    blank added

--- examples/form/src/App.js
+++ examples/form/src/App.js
@@ -11,7 +11,13 @@ const Form = () => {
                 <div className="mb-3">
                     <input value={text} type="text" className="form-control" readOnly />
                     <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
-                    <input onChange={(e) => setText(e.target.value)} type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                    <input
+                        onChange={(e) => setText(e.target.value)}
+                        type="email"
+                        value={text}
+                        className={`form-control ${color}`}
+                        id="exampleFormControlInput1"
+                        placeholder="name@example.com" />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
