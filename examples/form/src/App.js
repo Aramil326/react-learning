9f6e11824bf89b80ed02e00579d238a24aee25b0
Shaimardanov Aramil <aramil28022005@gmail.com>
2022-09-08 18:22:24 +0300

    lesson 169

--- examples/form/src/App.js
+++ examples/form/src/App.js
@@ -2,26 +2,49 @@ import { useState, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import './App.css';
 
+function useInputWithValidate(initialValue) {
+    const [value, setValue] = useState(initialValue);
+
+    const onChange = event => {
+        setValue(event.target.value)
+    }
+
+    const validateInput = () => {
+        return value.search(/\d/) >= 0
+    }
+
+    return { value, onChange, validateInput }
+}
+
 const Form = () => {
-    const [text, setText] = useState('');
+
+    const input = useInputWithValidate('');
+    const textArea = useInputWithValidate('');
+
+    const color = input.validateInput() ? 'text-danger' : null
 
     return (
         <Container>
             <form className="w-50 border mt-5 p-3 m-auto">
                 <div className="mb-3">
-                    <input value={text} type="text" className="form-control" readOnly />
+                    <input value={`${input.value} / ${textArea.value}`} type="text" className="form-control" readOnly />
                     <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
                     <input
-                        onChange={(e) => setText(e.target.value)}
+                        onChange={input.onChange}
                         type="email"
-                        value={text}
+                        value={input.value}
                         className={`form-control ${color}`}
                         id="exampleFormControlInput1"
                         placeholder="name@example.com" />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    <textarea
+                        onChange={textArea.onChange}
+                        value={textArea.value}
+                        className="form-control"
+                        id="exampleFormControlTextarea1"
+                        rows="3"></textarea>
                 </div>
             </form>
         </Container>
